28a787715c5c5fd61df003bb8a54ab58bad99a11
RobinGTodd <31745329+RobinGTodd@users.noreply.github.com>
2023-02-07 00:25:32 +0000

    Feb 6 class- Constants

--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "expressions.h"
 #include "casting.h"
+#include "constants.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -17,14 +18,19 @@ TEST_CASE("Verify operator precedence 2", "verify order of operations")
 	REQUIRE(operator_precedence_2(3, 6, 3) == 3);
 }
 
-TEST_CASE("Verify int to double", "verify w multiplication")
-{
-	REQUIRE(convert_to_double(10, .59) == 5.90);
-}
+// TEST_CASE("Verify int to double", "verify w multiplication")
+// {
+// 	// REQUIRE(convert_to_double(10, .59) == 5.90);
+// }
 
 TEST_CASE("Verify double to int", "echo")
 {
-	REQUIRE(convert_double_to_int(5.55) == 5);
+	// REQUIRE(convert_double_to_int(5.55) == 5);
 	// REQUIRE(convert_double_to_int(5.55) == 6);
 
+}
+
+TEST_CASE("Test const variable get area of circle")
+{
+	REQUIRE(get_area_of_circle(10)==314.159);
 }
