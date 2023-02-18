458ac31fec6e89cb872c186e525c990f366a43f1
RobinGTodd <31745329+RobinGTodd@users.noreply.github.com>
2023-02-18 20:26:52 +0000

    Feb 15 Clas

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,14 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 0;
+    int &num_ref = num;
+
+    cout<<"Num is: "<<num<<"\n";
+    cout<<"Stack address of num: "<<&num<<"\n";
+
     return 0;
 }
